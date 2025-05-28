362219211720eb243ff329bfe0f899236fb58acc
eric moslow <eric.moslow@kellogg.northwestern.edu>
2025-05-28 03:45:48 +0000

    built users controller requiring bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,25 @@ class UsersController < ApplicationController
   def new
   end
 
+  require 'bcrypt'
+
   def create
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
-    @user.save
-    redirect_to "/"
+    @user["password"] =  BCrypt::Password.create(params["password"])
+
+if @user["username"].blank?
+  flash["notice"] = "Username is Required"
+  redirect_to "/users/new" and return
+end
+
+if @user.save
+  session["user_id"] = @user.id 
+  redirect_to "/"
+
+else flash["notice"] = "Signup Failed."
+  redirect_to "/users/new"
+end
   end
 end
