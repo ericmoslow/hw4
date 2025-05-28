6a36ddf754cf684c17e4b298eb9a3f903e359cbb
eric moslow <eric.moslow@kellogg.northwestern.edu>
2025-05-28 03:57:49 +0000

    sessions controller accoutn validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" =>params["email"]})
+    if 
+      @user && BCrypt::Password.new(@user["password"] == params["password"])
+      session["user_id"] = @user["id"]
+      flash["notice"] = "You've logged in Successfully!"
+      redirect_to "/places"
+    else
+      flash["notice"] = "Invalid password or email."
+      redirect_to "/login"
+    end
   end
 
   def destroy
