0464c2465d62aaf408cf555c73ea30a9f6d20000
eric moslow <eric.moslow@kellogg.northwestern.edu>
2025-05-28 03:59:59 +0000

    added logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     @user = User.find_by({"email" =>params["email"]})
     if 
       @user && BCrypt::Password.new(@user["password"] == params["password"])
-      session["user_id"] = @user["id"]
-      flash["notice"] = "You've logged in Successfully!"
+      session["user_id"] = @user.id
+      flash["notice"] = "You've logged in successfully!"
       redirect_to "/places"
     else
       flash["notice"] = "Invalid password or email."
@@ -16,6 +16,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "You've logged out successfully!"
+    redirect_to "/login"
   end
 end
   
