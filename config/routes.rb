c0aba3914ac6fa598afc83e1c3c91679ead0baed
eric moslow <eric.moslow@kellogg.northwestern.edu>
2025-05-28 03:51:29 +0000

    wrote necessary login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  delete "/logout", to: "sessions#destroy"
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
 end
